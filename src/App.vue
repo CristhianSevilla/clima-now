ab2cefe316b80d341d1526dfeb3fee08c0adb3ce
Cristhian Sevilla <crissev17@gmail.com>
2023-11-13 15:54:09 -0600

    Diseño y Logo

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,10 @@ import Formulario from "./components/Formulario.vue";
 </script>
 
 <template>
-  <h1 class="titulo">Buscador de clima</h1>
+  <div class="header">
+    <img src="../public/favi.svg" alt="Logo" />
+    <h1 class="titulo"><Span>ClimaNow </Span> Buscador de clima</h1>
+  </div>
   <div class="contenedor buscador-clima">
     <Formulario />
   </div>
