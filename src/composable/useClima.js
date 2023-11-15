0adbc7560564d13ffb85db27ab1cc311d25d61ca
Cristhian Sevilla <crissev17@gmail.com>
2023-11-15 16:36:50 -0600

    Consultando el clima

--- src/composable/useClima.js
+++ src/composable/useClima.js
@@ -1,15 +1,19 @@
 import axios from "axios";
 
 export default function useClima() {
-  const obtenerClima = (ciudad) => {
+  const obtenerClima = async (ciudad) => {
     console.log(ciudad);
     //Importar el api key
     const key = import.meta.env.VITE_API_KEY;
 
-    const urlCiudad = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${key}&units=metric`;
-
-    console.log(urlCiudad);
-
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${key}&units=metric`;
+      const { data } = await axios(url);
+      console.log(url);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
     //obtener el clima
   };
 
