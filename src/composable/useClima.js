3db280b5db9b2134a83a0ad078c8fb3ffde22da3
Cristhian Sevilla <crissev17@gmail.com>
2023-11-15 17:26:30 -0600

    Agregando un spinner

--- src/composable/useClima.js
+++ src/composable/useClima.js
@@ -3,20 +3,25 @@ import axios from "axios";
 
 const clima = ref({});
 
+const cargando = ref(false);
+
 export default function useClima() {
   const obtenerClima = async (ciudad) => {
     console.log(ciudad);
     //Importar el api key
     const key = import.meta.env.VITE_API_KEY;
+    cargando.value = true;
+    clima.value = {};
 
     try {
       //obtener el clima
-
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${key}&units=metric`;
       const { data } = await axios(url);
       clima.value = data;
     } catch (error) {
       console.log(error);
+    } finally {
+      cargando.value = false;
     }
   };
 
@@ -33,5 +38,6 @@ export default function useClima() {
     clima,
     mostrarClima,
     formatearTemperatura,
+    cargando,
   };
 }
